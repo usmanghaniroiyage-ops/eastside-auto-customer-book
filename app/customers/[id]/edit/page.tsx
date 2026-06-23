23ca30a06d68162b0ba9e2bace16b1b8a5905029
Usman <usmancraft73@gmail.com>
2026-06-23 14:05:18 +0100

    fix: force dynamic on edit page

--- app/customers/[id]/edit/page.tsx
+++ app/customers/[id]/edit/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { createClient } from "@/lib/supabase/server";
 import { updateCustomer } from "../../actions";
 import { redirect } from "next/navigation";
